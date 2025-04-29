e8ca0cafccb3f37fbb1949977a75fe03827c0c69
Dávilos <daviloscostagg@hotmail.com>
2025-04-29 09:37:30 -0300

    feat: add the JwtStrategy to providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,24 +1,30 @@
 import { Module } from "@nestjs/common";
-import { AuthController } from "./auth.controller";
-import { AuthService } from "./auth.service";
 import { PassportModule } from "@nestjs/passport";
 import { JwtModule } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config";
 import { Env } from "src/env";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
 	imports: [
 		PassportModule,
 		JwtModule.registerAsync({
 			inject: [ConfigService],
+			global: true,
 			useFactory(config: ConfigService<Env, true>) {
+				const privateKey = config.get("JWT_PRIVATE_KEY", { infer: true });
+				const publicKey = config.get("JWT_PUBLIC_KEY", { infer: true });
+
 				return {
-					secret: config.get("JWT_SECRET", { infer: true }),
+					privateKey: Buffer.from(privateKey, "base64"),
+					publicKey: Buffer.from(publicKey, "base64"),
+					signOptions: {
+						algorithm: "RS256",
+					},
 				};
 			},
 		}),
 	],
-	controllers: [AuthController],
-	providers: [AuthService],
+	providers: [JwtStrategy],
 })
 export class AuthModule {}
