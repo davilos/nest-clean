1512a6f0f060f1ecb58dbf5af7743c37907a6863
Dávilos <daviloscostagg@hotmail.com>
2025-05-07 12:13:56 -0300

    refactor: implements clean architecture folder structure

---
+++ src/infra/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from "@nestjs/common";
+import { PrismaService } from "./prisma/prisma.service";
+import { CreateAccountController } from "./http/controllers/create-account.controller";
+import { ConfigModule } from "@nestjs/config";
+import { envSchema } from "./env";
+import { AuthModule } from "./auth/auth.module";
+import { AuthenticateController } from "./http/controllers/authenticate.controller";
+import { CreateQuestionController } from "./http/controllers/create-question.controller";
+import { FetchRecentQuestionsController } from "./http/controllers/fetch-recent-questions.controller";
+
+@Module({
+	imports: [
+		ConfigModule.forRoot({
+			validate: (config) => envSchema.parse(config),
+			isGlobal: true,
+			cache: true,
+		}),
+		AuthModule,
+	],
+	controllers: [
+		CreateAccountController,
+		AuthenticateController,
+		CreateQuestionController,
+		FetchRecentQuestionsController,
+	],
+	providers: [PrismaService],
+})
+export class AppModule {}
