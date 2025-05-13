c63973147aa85c220c2cd0cb9113eeb4c0870391
Dávilos <daviloscostagg@hotmail.com>
2025-05-13 12:07:10 -0300

    feat: implements the findById method using the mapper

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -3,12 +3,25 @@ import { QuestionsRepository } from "@/domain/forum/application/repositories/que
 import { Question } from "@/domain/forum/enterprise/entities/question";
 
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma.service";
+import { PrismaQuestionMapper } from "../mappers/prisma-question-mapper";
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
-	findById(id: string): Promise<Question | null> {
-		throw new Error("Method not implemented.");
+	constructor(private prismaService: PrismaService) {}
+
+	async findById(id: string): Promise<Question | null> {
+		const question = await this.prismaService.question.findUnique({
+			where: {
+				id,
+			},
+		});
+
+		if (!question) return null;
+
+		return PrismaQuestionMapper.toDomain(question);
 	}
+
 	findBySlug(slug: string): Promise<Question | null> {
 		throw new Error("Method not implemented.");
 	}
