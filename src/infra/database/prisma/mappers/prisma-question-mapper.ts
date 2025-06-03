f11be49ea749bf572c12882d056524bb4c12bbaf
Dávilos Tavares da Costa <daviloscostagg@hotmail.com>
2025-06-03 13:31:47 -0300

    feat: add a function that mappers to Prisma

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,7 +1,8 @@
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
-import { Question as PrismaQuestion } from "@prisma/client";
+
+import { Question as PrismaQuestion, Prisma } from "@prisma/client";
 
 export class PrismaQuestionMapper {
 	static toDomain(raw: PrismaQuestion): Question {
@@ -22,5 +23,18 @@ export class PrismaQuestionMapper {
 			},
 			questionId,
 		);
+	}
+
+	static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+		return {
+			id: question.id.toString(),
+			authorId: question.authorId.toString(),
+			title: question.title,
+			content: question.content,
+			slug: question.slug.value,
+			bestAnswerId: question.bestAnswerId?.toString(),
+			createdAt: question.createdAt,
+			updatedAt: question.updatedAt,
+		};
 	}
 }
