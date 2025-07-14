e955421f157f9847c2eea113351687ae014478d4
Dávilos Tavares da Costa <daviloscostagg@hotmail.com>
2025-07-14 17:10:12 -0300

    refactor: refactor the modules

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,22 +1,28 @@
+import { AuthenticateStudentUseCase } from "@/domain/forum/application/use-cases/authenticate-student";
+import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
+import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions";
+import { RegisterStudentUseCase } from "@/domain/forum/application/use-cases/register-student";
 import { Module } from "@nestjs/common";
-
-import { CreateAccountController } from "./controllers/create-account.controller";
+import { CryptographyModule } from "../cryptography/cryptography.module";
+import { DatabaseModule } from "../database/database.module";
 import { AuthenticateController } from "./controllers/authenticate.controller";
+import { CreateAccountController } from "./controllers/create-account.controller";
 import { CreateQuestionController } from "./controllers/create-question.controller";
 import { FetchRecentQuestionsController } from "./controllers/fetch-recent-questions.controller";
 
-import { DatabaseModule } from "../database/database.module";
-import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
-import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions";
-
 @Module({
-	imports: [DatabaseModule],
-	controllers: [
-		CreateAccountController,
-		AuthenticateController,
-		CreateQuestionController,
-		FetchRecentQuestionsController,
-	],
-	providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase],
+  imports: [DatabaseModule, CryptographyModule],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+    FetchRecentQuestionsController,
+  ],
+  providers: [
+    CreateQuestionUseCase,
+    FetchRecentQuestionsUseCase,
+    RegisterStudentUseCase,
+    AuthenticateStudentUseCase,
+  ],
 })
 export class HttpModule {}
